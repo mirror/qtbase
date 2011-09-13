1f0888761b2eeef1bed5e49ab849519c8b9f06fc
Richard Moore <rich@kde.org>
2011-09-13 22:02:41 +0100

    Change copyright to use the right name.

--- src/network/ssl/qsslcertificateextension.cpp
+++ src/network/ssl/qsslcertificateextension.cpp
@@ -1,6 +1,6 @@
 /****************************************************************************
 **
-** Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
+** Copyright (C) 2011 Richard J. Moore <rich@kde.org>
 ** All rights reserved.
 ** Contact: Nokia Corporation (qt-info@nokia.com)
 **

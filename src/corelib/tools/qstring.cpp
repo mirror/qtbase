70fea3ed9986baee4b25347cc946a4e01d0a4627
Konstantin Ritt <ritt.ks@gmail.com>
2011-10-07 19:17:09 +0400

    optimize QString::toLower()/toUpper() for special cases, step 2
    from now, QUnicodeTables::specialCaseMap[] starts with a placeholder; so,
    if somethingCaseSpecial is true, then somethingCaseDiff is always greater than 0

--- src/corelib/tools/qstring.cpp
+++ src/corelib/tools/qstring.cpp
@@ -4914,7 +4914,7 @@ QString QString::toLower() const
         } else {
             prop = qGetProp(*p);
         }
-        if (prop->lowerCaseDiff || prop->lowerCaseSpecial) {
+        if (prop->lowerCaseDiff) {
             if (QChar::isLowSurrogate(*p))
                 --p; // safe; diff is 0 for surrogates
             QString s(d->size, Qt::Uninitialized);
@@ -4975,7 +4975,7 @@ QString QString::toCaseFolded() const
         } else {
             prop = qGetProp(*p);
         }
-        if (prop->caseFoldDiff || prop->caseFoldSpecial) {
+        if (prop->caseFoldDiff) {
             if (QChar::isLowSurrogate(*p))
                 --p; // safe; diff is 0 for surrogates
             QString s(d->size, Qt::Uninitialized);
@@ -5036,7 +5036,7 @@ QString QString::toUpper() const
         } else {
             prop = qGetProp(*p);
         }
-        if (prop->upperCaseDiff || prop->upperCaseSpecial) {
+        if (prop->upperCaseDiff) {
             if (QChar::isLowSurrogate(*p))
                 --p; // safe; diff is 0 for surrogates
             QString s(d->size, Qt::Uninitialized);

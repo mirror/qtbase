6bb9d0dff85eeaf08fc90dfa6485ba759ede6d82
Paul Olav Tvete <paul.tvete@digia.com>
2013-02-11 14:06:25 +0100

    Remove code that only makes sense on android
    Task-number: QTBUG-29532
    
    Change-Id: Id6750af9bf06cff21ef988446b9b6bc0da5addca
    Reviewed-by: Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@digia.com>

--- src/network/kernel/qnetworkproxy_win.cpp
+++ src/network/kernel/qnetworkproxy_win.cpp
@@ -514,8 +514,7 @@ QList<QNetworkProxy> QNetworkProxyFactory::systemProxyForQuery(const QNetworkPro
         // url could be empty, e.g. from QNetworkProxy::applicationProxy(), that's fine,
         // we'll still ask for the proxy.
         // But for a file url, we know we don't need one.
-        if (url.scheme() == QLatin1String("file") || url.scheme() == QLatin1String("qrc")
-                || url.scheme() == QLatin1String("assets"))
+        if (url.scheme() == QLatin1String("file") || url.scheme() == QLatin1String("qrc"))
             return sp->defaultResult;
         if (query.queryType() != QNetworkProxyQuery::UrlRequest) {
             // change the scheme to https, maybe it'll work
